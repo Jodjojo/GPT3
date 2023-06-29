d47f3951be7099b6cbb4d54ec4b7a0b8af158296
Joseph Yusuff <joseph.yusuff12@gmail.com>
2023-06-29 22:09:41 +0100

    Footer Section React Rudiments

--- src/Containers/blog/Blog.jsx
+++ src/Containers/blog/Blog.jsx
@@ -31,7 +31,7 @@ const Blog = () => {
 						title='GPT-3 and Open  AI is the future. Let us exlore how it is?'
 					/>
 					<Article
-						imgUrl={blog03}
+						imgUrl={blog04}
 						date='Sep 26, 2021'
 						title='GPT-3 and Open  AI is the future. Let us exlore how it is?'
 					/>
